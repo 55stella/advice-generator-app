c5b2fb3fb280fb5d9d5e3438df53e8269986ec7d
55stella <stellarichards7@gmail.com>
2022-04-14 14:24:05 +0100

    all

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+import {useState, useEffect} from 'react'
+import Dice from './images/icon-dice.svg'
+import Pattern from './images/pattern-divider-desktop.svg'
 
 function App() {
+  // const[adviceList, setAdviceList]= useState([
+  //   {id:1, body: 'A stitch in time savves nine'},
+  //   {id:2, body: 'Make hay while the sun shine'},
+  //   {id:3, body: 'Early to bed, Early to rise'},
+  //   {id:4, body: 'Problem e no dey finish, make you try dey enjoy'},
+  // ])
+  const [advice, setAdvice] = useState({})
+  // const [adviceIndex, setAdviceIndex] = useState(0)
+
+  // const generatRandom=()=> Math.floor(Math.random()*adviceList.length)
+  const getAdvice =async() =>{
+    let adviceSlip =   await fetch('https://api.adviceslip.com/advice')
+    console.log(adviceSlip)
+    return adviceSlip.json()
+  }
+  
+  const showAdvice =()=>{
+    getAdvice().then(data=>{
+    setAdvice(data.slip)
+      // console.log(showAdvice)
+      
+    
+    })
+  }
+  //  const next = () =>{
+  //    if(adviceIndex ===adviceList.length){
+  //      setAdviceIndex(0)
+  //    }
+  //    setAdviceIndex(adviceIndex +1)
+  //  }
+  useEffect(()=>{
+    // console.log(adviceList)
+
+    showAdvice()
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="card">
+        {/* <p>Advice #{adviceList[adviceIndex].id}</p> */}
+        <p>Advice #{advice.id}</p>
+        {/* <h1>"{adviceList[adviceIndex].body}"</h1> */}
+        <h1>"{advice.advice}"</h1>
+        {/* <button onClick ={()=>setAdviceIndex(generatRandom())}> New Advice</button> */}
+        <img src="{Pattern}"  className ='pattern'alt="" />
+        <button onClick ={showAdvice}> New Advice
+        <img src="{Dice}" alt="" />
+        </button>
+      </div>
     </div>
   );
 }
